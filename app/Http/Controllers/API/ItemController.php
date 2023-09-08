12e8652b71b252cc21fd0530777ae8cb74bd501c
Rifqi Haidar <jurig.blogger@gmail.com>
2023-09-08 16:12:42 +0700

    feat: add show by id and search by name, rename 'index' to 'show'

--- app/Http/Controllers/API/ItemController.php
+++ app/Http/Controllers/API/ItemController.php
@@ -8,13 +8,62 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    public function index()
+    public function show()
     {
         $items = Items::all();
+
+        if ($items->isEmpty()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Tidak ada item'
+            ], 400);
+        }
+
         return response()->json([
-            'message' => 'item q',
-            'data' => $items 
-        ]);
+            'status' => true,
+            'data' => $items
+        ], 200);
+    }
+
+    public function showById($id)
+    {
+        $item = Items::find($id);
+
+        if (!$item) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Item tidak ditemukan'
+            ], 400);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $item
+        ], 200);
+    }
+
+    public function searchByName(Request $request)
+    {
+        if (!$request->name) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Parameter `name` ditemukan'
+            ], 400);
+        }
+
+        $search = $request->name;
+        $items = Items::where("name", "LIKE", "%{$search}%")->get();
+        if ($items->isEmpty()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Tidak ada item `'.$search.'` ditemukan'
+            ], 400);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $items
+        ], 200);
     }
 
     public function store(Request $request){
@@ -38,7 +87,7 @@ class ItemController extends Controller
         ]);
     }
     public function update(Request $request,$id){
-        
+
         $item = Items::find($id);
 
         if(!$item){
@@ -53,20 +102,20 @@ class ItemController extends Controller
             'stock' => 'required',
             'borrowed' => 'required'
         ]);
-        
+
          $item->name = $attrs['name'];
          $item->lab_id =  $attrs['lab'];
          $item->jenis = $attrs['jenis'];
          $item->stock = $attrs['stock'];
          $item->borrowed = $attrs['borrowed'];
-     
+
         return response()->json([
             'message' => 'berhasil',
             'data' => $item
         ]);
     }
     // public function destroy($id){
-        
+
     //     $item = Items::find($id);
 
     //     if(!$item){
@@ -75,26 +124,24 @@ class ItemController extends Controller
     //         ]);
     //     }
     //      $item->delete();
-     
+
     //     return response()->json([
     //         'message' => 'berhasil dihaps',
     //     ]);
     // }
-        function delete($id)  {
-            $item = Items::find($id);
+    function delete($id)  {
+        $item = Items::find($id);
 
-            if (!$item){
-                return response()->json([
-                    'status'=> false,
-                    'message' => 'Item Tidak Ditemukan'
-                ]);
-
-            }
-            $item->delete();
+        if (!$item){
             return response()->json([
-                'message' => 'Item Berhasil Dihapus'
+                'status'=> false,
+                'message' => 'Item Tidak Ditemukan'
             ]);
+
         }
-
-
+        $item->delete();
+        return response()->json([
+            'message' => 'Item Berhasil Dihapus'
+        ]);
+    }
 }
