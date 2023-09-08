19487e699cd890cf134be85f009378ef5d5e8670
kurumi64 <adipamungkas.kiki@gmail.com>
2023-09-08 11:22:24 +0700

    Add Loan Function

--- app/Http/Controllers/API/LoanController.php
+++ app/Http/Controllers/API/LoanController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoanController extends Controller
 {
-    //
+    private function history_read()
+    {
+        $history = history::all();
+
+        if (!$history && Auth::user()->name)
+        {
+            return response()->json(
+            [
+                'status' => 'failed',
+                'error_pesan' => 'TIDAK ADA HISTORY',
+            ]);
+        }
+
+        return response()->json($history, 200);
+    } 
 }
